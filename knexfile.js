59d4a97ee7713c1dc889b4616cae824564ccf9f6
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-12-05 14:13:38 -0300

    :bricks: backend done

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { Database } = require("sqlite");
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "Database.db"),
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
+const movieNotesRoutes = require("./notes.routes");
+const movieTagsRoutes = require("./tags.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
+routes.use("/movie_notes", movieNotesRoutes);
+routes.use("/movie_tags", movieTagsRoutes);
 
 module.exports = routes;
